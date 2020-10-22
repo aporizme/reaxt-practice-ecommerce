a7781c0f2e41664283236c04a1578a5b34bfca9f
unknown <carlosjester85@yahoo.com>
2020-10-22 20:43:05 +0800

    first

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,11 @@
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -14,6 +15,7 @@ function Checkout() {
           alt=""
         />
         <div>
+          <h3>{user?.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { auth } from "./firebase";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./Login.css";
 function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+  const register = (e) => {
+    //register
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -16,19 +45,33 @@ function Login() {
 
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button>Sign In</button>
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
         </form>
         <p>
           Lorem ipsum Loremg elements must have an alt prop, either with
           meaningful text, or an empty string for decorative images mg elements
           must have an alt prop
         </p>
-        <button className="login__registerButton">
+        <button onClick={register} className="login__registerButton">
           Create your Amazon Account
         </button>
       </div>
